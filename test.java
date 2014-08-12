6521514726804636cd3a173d7b2c72e5584a1450
Guillermo <gach151195@gmail.com>
2014-08-12 17:12:36 -0600

    peque cambio

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public class test{
 	public static void main (String[] args){
 		ArrayList c = new ArrayList();
 		//System.out.println("El area del circulo es: "+c.calcularArea());
-		for (int i=0;i<8;i++){
+		for (int i=0;i<7;i++){
 			System.out.println(i);
 			c.add(i);
 			}
@@ -12,8 +12,12 @@ public class test{
 		System.out.println(c.find(666));
 		System.out.println(c.find(6));
 		System.out.println(c.getSize());
-		System.out.println(c.getElemento(8));
+		System.out.println(c.getElemento(3));
 		System.out.println(c.getMax_size());
+		System.out.println(c.find(2));
+		c.insert(65,2);
+		System.out.println(c.find(2));
+
 		}
 	
 	
